566e4955d8c9a6d0c57ab50d95073e476ce84b28
Aleksander Grzybowski <kelog@orange.pl>
2015-04-24 11:23:08 +0200

    Hack for handling tab in textarea

--- web-app/js/app.js
+++ web-app/js/app.js
@@ -20,6 +20,28 @@ $('#send').click(function () {
 
 });
 
+$("textarea").keydown(function(e) {
+    if(e.keyCode === 9) { // tab was pressed
+        // get caret position/selection
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        var $this = $(this);
+        var value = $this.val();
+
+        // set textarea value to: text before caret + tab + text after caret
+        $this.val(value.substring(0, start)
+        + "\t"
+        + value.substring(end));
+
+        // put caret at right position again (add one for the tab)
+        this.selectionStart = this.selectionEnd = start + 1;
+
+        // prevent the focus lose
+        e.preventDefault();
+    }
+});
+
 function displayError(errorString) {
     $('#chart').remove();
     var newDiv = $('<div></div>');
@@ -69,5 +91,6 @@ function displayChart(data) {
     });
 
 }
+
 
 
