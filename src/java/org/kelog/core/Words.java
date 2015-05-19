983d007d381f38ba87ffa6480902a349b9999725
Aleksander Grzybowski <kelog@orange.pl>
2015-05-19 19:16:26 +0200

    More words, skipping commented out lines

--- src/java/org/kelog/core/Words.java
+++ src/java/org/kelog/core/Words.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -19,10 +20,21 @@ public class Words {
 		logger.info("Loading list of words");
 		try {
 			list = Files.readAllLines(new File(Config.WORDS_FILENAME).toPath(), Charset.defaultCharset());
+            list = stripComments(list); // no streams here...
 			logger.info("List of words loaded, count: " + list.size());
 		} catch (IOException e) {
 			logger.warning("Error creating words list " + e);
 			throw new WordsListNotFoundException();
 		}
 	}
+
+    private static List<String> stripComments(List<String> list) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String word : list) {
+            if (!word.startsWith("#")) {
+                result.add(word);
+            }
+        }
+        return result;
+    }
 }
