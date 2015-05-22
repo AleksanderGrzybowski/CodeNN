56515989a2ea4b60049aeee49d7def16223b9ea1
Aleksander Grzybowski <kelog@orange.pl>
2015-05-22 10:18:59 +0200

    Fixes before presentation - smaller error, words sorted

--- src/java/org/kelog/end/Config.java
+++ src/java/org/kelog/end/Config.java
@@ -7,5 +7,5 @@ public class Config {
 	public static final String WORDS_FILENAME= TRAINING_DATA_DIRECTORY + File.separator + "words.txt";
 	public static final String NETWORK_FILENAME = "/tmp/network.eg";
 	public static final int MAXIMUM_EPOCHS = 10000;
-	public static final double MAXIMUM_ERROR = 0.00001;
+	public static final double MAXIMUM_ERROR = 0.001;
 }
