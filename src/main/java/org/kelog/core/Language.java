f624e24517b4028db2d0fbf933e1bda7e5acc491
Aleksander Grzybowski <kelog@orange.pl>
2015-09-09 17:12:39 +0200

    Comment

--- src/main/java/org/kelog/core/Language.java
+++ src/main/java/org/kelog/core/Language.java
@@ -1,5 +1,9 @@
 package org.kelog.core;
 
+/**
+ * DO NOT ALTER SEQUENCE (lot of stuff relies on that).
+ * You can add at the end, however.
+ */
 public enum Language {
 	BASH, C, CPP, CSHARP, JAVA, JAVASCRIPT, PASCAL, PERL, PYTHON, RUBY
 }
